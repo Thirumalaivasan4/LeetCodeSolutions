6f09ad8e16776dbaa0fa83905509e3c4f137e435
THIRUMALAIVASAN.S <138312185+Thirumalaivasan4@users.noreply.github.com>
2024-06-18 10:40:13 +0530

    Time: 2 ms (97.14%), Space: 43.2 MB (68.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] charCounts = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            charCounts[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            charCounts[c - 'a']--;
+        }
+
+        for (int count : charCounts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
