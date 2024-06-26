d543bfb429b9e5a69e93e4071d7d679e29663125
THIRUMALAIVASAN.S <138312185+Thirumalaivasan4@users.noreply.github.com>
2024-06-26 14:35:15 +0530

    Time: 0 ms (100%), Space: 40 MB (65.91%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getSum(int a, int b)
+    {
+        while(b!=0){
+          int ans=a^b;
+          int carry=(a&b)<<1;
+          a=ans;
+          b=carry;
+        }
+        return a;
+        
+    }
+}
