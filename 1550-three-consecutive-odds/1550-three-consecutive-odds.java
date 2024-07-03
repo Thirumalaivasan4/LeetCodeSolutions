990163a793a239919e38b34e584b4ce301795249
THIRUMALAIVASAN.S <138312185+Thirumalaivasan4@users.noreply.github.com>
2024-07-03 14:50:14 +0530

    Time: 0 ms (100%), Space: 42.7 MB (6.37%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -6,7 +6,7 @@ class Solution {
     else{
         for(int i=0;i<arr.length-2;i++){
            if(arr[i]%2!=0&&arr[i+1]%2!=0&&arr[i+2]%2!=0){
-            return true;
+               return true;
            } 
         }
         return false;
