fbccb05d9b1e6e921edbe0e5ac39f0918e02f1a2
THIRUMALAIVASAN.S <138312185+Thirumalaivasan4@users.noreply.github.com>
2024-07-05 15:56:59 +0530

    Time: 8 ms (90.89%), Space: 45.4 MB (73.4%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,26 +1,20 @@
 class Solution {
-    public int minimumPushes(String word) {
-        Map<Character, Integer> charFreq = new HashMap<>();
-        for (char c : word.toCharArray()) 
-            charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
+  public int minimumPushes(String word) {
+    int ans = 0;
 
-        TreeMap<Integer, List<Character>> sortedMap = new TreeMap<>(Collections.reverseOrder());
-		// Sorting Characters by Frequency (sortedMap)
-        for (char c : charFreq.keySet()) {
-            int freq = charFreq.get(c);
-            sortedMap.computeIfAbsent(freq, k -> new ArrayList<>()).add(c);
-        }
+    int[] count = new int[26];
 
-        int pushes = 0, count = 0;
-        for (Map.Entry<Integer, List<Character>> entry : sortedMap.entrySet()) {
-            List<Character> chars = entry.getValue();
-            int freq = entry.getKey();
-            for (int i = 0; i < chars.size(); i++) {
-                count++;
-                pushes += freq * (count > 24 ? 4: (count > 16 ? 3:(count > 8  ? 2: 1))); 
-            }
-        }
 
-        return pushes;
-    }
+    for (final char c : word.toCharArray())
+              ++count[c - 'a'];
+
+    Arrays.sort(count);
+
+    for (int i = 0; i < 26; ++i)
+
+      ans += count[26 - i - 1] * (i / 8 + 1);
+      
+      return ans ;
+  } 
+     
 }
