1625520879fb1000c8ee4453cbcd9e884d0a0435
THIRUMALAIVASAN.S <138312185+Thirumalaivasan4@users.noreply.github.com>
2024-07-07 00:00:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,13 +4,7 @@ class Solution {
 
         }
         int arr[]=new int[n+1];
-         if(n==0){
-           return 0;  
-         }
-          if(n==1)
-          {
-           return 1;  
-         }
+         
        arr[0]=0;
        arr[1]=1;
        for(int i=2;i<n+1;i++)
